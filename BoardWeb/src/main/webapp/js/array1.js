cdd61dcf6162228a1d9f0dcd44a28b3a2ff2d590
wns3915 <wns3915@naver.com>
2024-07-25 11:56:27 +0900

    7월25일 자바스크립트 복습

--- BoardWeb/src/main/webapp/js/array1.js
+++ BoardWeb/src/main/webapp/js/array1.js
@@ -2,26 +2,45 @@
  *  array1.js
  */
 //thead에 있는 체크박스 이벤트 등록
-document.querySelector('div.container-fluid >table>thead input[type="checkbox"]')//
-.addEventListener('change',changeFnc);
+document.querySelector('div.container-fluid>table>thead input[type="checkbox"]')//
+	.addEventListener('change', chaout);
 
-function changeFnc(e){
+
+
+function changeFnc(e) {
+	console.log(e.target.checked);
+	console.log(document.querySelectorAll('div.container-fluid >table>tbody input[type="checkbox"]:checked')
+		.length);
 	//찾으려는 대상 체크박스.
 	document.querySelectorAll('div.container-fluid >table>tbody input[type="checkbox"]')//
-	.forEach(function(item){
-		console.log(item);
-	})
+		.forEach(function(item) {
+			console.log(item);
+			item.checked = e.target.checked;
+		})
+}// end of changeFnc(e)
+
+
+//전체에서 하나 빼면 전체체크 해제 하기
+function chaout(e) {
+	let num1 = document.querySelectorAll('div.container-fluid >table>tbody input[type="checkbox"]').length;
+	let num2 = document.querySelectorAll('div.container-fluid >table>tbody input[type="checkbox"]:checked').length;
+	if (num1 == num2) {
+		document.querySelector('div.container-fluid>table>thead input[type="checkbox"]').checked = true;
+	} else {
+		false;
+	}
 }
 
+
 //수정 버튼에 클릭이벤트.
-document.getElementById('modBtn').addEventListener('click',modBtnFnc );
+document.getElementById('modBtn').addEventListener('click', modBtnFnc);
 //modBtnFnc 이벤트 핸들러.
-function modBtnFnc(e){
-	
-	document.querySelectorAll().forEach(function(e){
-		if(tr.children[0].innerHTML == document.querySelector('#fname').value){
-		   tr.children[1].innerHTML == document.querySelector('#faddress');
-		   tr.children[2].innerHTML == document.querySelector('#height');
+function modBtnFnc(e) {
+
+	document.querySelectorAll().forEach(function(e) {
+		if (tr.children[0].innerHTML == document.querySelector('#fname').value) {
+			tr.children[1].innerHTML == document.querySelector('#faddress');
+			tr.children[2].innerHTML == document.querySelector('#height');
 		}
 	})
 }
@@ -33,39 +52,40 @@ const friends = [
 	{ name: "김수영", address: "평택 300번지", height: 179 },
 ]
 
-makeList();
 
-function makeList(){
-	friends.forEach(function(friend){
+makeList();
+function makeList() {
+	friends.forEach(function(friend) {
 		let tr = makeTr(friend);
 		document.getElementById('list').appendChild(tr);
 	})
 }
+
 //등록 버튼에 클릭 이벤트 추가
-document.getElementById('addBtn').addEventListener('click',addBtnFnc);
+document.getElementById('addBtn').addEventListener('click', addBtnFnc);
 
 //addBtnFnc 이벤트핸들러
-function addBtnFnc(e){
+function addBtnFnc(e) {
 	let name = document.getElementById('fname').value;
 	let address = document.getElementById('faddress').value;
 	let height = document.getElementById('height').value;
-	if(!name || !address || !height){
+	if (!name || !address || !height) {
 		alert('값을 입력하시오');
 		return;
 	}
-	let friend = {name,address,height};
-	
+	let friend = { name, address, height };
+
 	let tr = makeTr(friend);
 	document.getElementById('list').appendChild(tr);
-	
+
 	alert('OK');
-	fname.value='';
-	faddress.value='';
-	document.getElementById('height').value ='';
+	fname.value = '';
+	faddress.value = '';
+	document.getElementById('height').value = '';
 };
 
 
-function detailCallback(e){
+function detailCallback(e) {
 	e.stopPropagation();
 	console.log(e.target.parentElement);
 	let tr = e.target.parentElement; // 이벤트 대상으로 tr영역을 찾아야함.
@@ -84,21 +104,21 @@ function detailCallback(e){
 
 
 //friend => tr 생성
-function makeTr(friend = {name:'Hong',address: 'Seoul',height:170}){
-	
+function makeTr(friend = { name: 'Hong', address: 'Seoul', height: 170 }) {
+
 	//tr 만드는 부분
 	let tr = document.createElement('tr');
-	tr.addEventListener('click',detailCallback, false);
-	
-	
-	for(let prop in friend){
+	tr.addEventListener('click', detailCallback, false);
+
+
+	for (let prop in friend) {
 		let td = document.createElement('td');
 		td.innerHTML = friend[prop];
 		tr.appendChild(td);
 	}
-	
-	
-	
+
+
+
 	//삭제버튼
 	let td = document.createElement('td');
 	let btn = document.createElement('button');
@@ -107,20 +127,27 @@ function makeTr(friend = {name:'Hong',address: 'Seoul',height:170}){
 		console.log(e);
 		e.stopPropagation(); //click : button > td > tr > table....
 		e.target.parentElement.parentElement.remove();
-	},false);
+	}, false);
 	btn.innerHTML = '삭제';
 	td.appendChild(btn);
 	tr.appendChild(td);
-	
-	
+
+
 	//체크박스 완성된 html은 --> <td><input type="checkbox"></td> 의 상위요소 tr에 붙히기.
 	td = document.createElement('td');
 	let inp = document.createElement('input');
 	inp.setAttribute('type', 'checkbox');
+	inp.addEventListener('change', chaout)
 	td.appendChild(inp);
 	tr.appendChild(td);
-	
-    return tr;
+
+	return tr;
+
+	//선택삭제
+	function delBtn() {
+
+	}
+
 }
 
 
